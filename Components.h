aba291f552a42663f3602cbaeaff2ca81b9659f4
albertobejaranor125 <120492373+albertobejaranor125@users.noreply.github.com>
2025-07-08 15:23:35 +0200

    Add files via upload

---
+++ Components.h
@@ -0,0 +1,32 @@
+#pragma once
+#include <string>
+#include <vector>
+
+struct Position {
+	int x = 0;
+	int y = 0;
+};
+
+struct Health {
+	int current = 100;
+	int max = 100;
+};
+struct Name
+{
+	std::string value;
+};
+struct Animation {
+	std::string currentState = "idle";
+	//std::vector<std::string> states;
+	int currentFrame = 0;
+	int maxFrames = 4;
+	float frameTime = 0.2f;
+	float timer = 0.0f;
+};
+struct Input {
+	std::string lastComand;
+};
+struct Collider {
+	int width = 1;
+	int height = 1;
+};
